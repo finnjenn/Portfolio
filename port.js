9d86973d32fadb6ffd2499e1f6be8382a885a430
finnjenn <finnjennn@gmail.com>
2023-05-15 16:42:35 -0400

    first commit

---
+++ port.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('#header');
+const subheader = document.querySelector('#subheader');
+const navbar = document.querySelector('.navbar');
+window.addEventListener('load', () => {
+  header.classList.add('show');
+  subheader.classList.add('show');
+  navbar.classList.add('show');
+});
